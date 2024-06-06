0a170978b0eb94532bba24b54d1f6549b878440f
Maylan <maylan.gomes@epitech.eu>
2024-06-06 11:23:34 +0200

    site url

--- src/app/api/movies/route.ts
+++ src/app/api/movies/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import movies from "/home/runner/work/cinema-test/cinema-test/public/data/movies.json";
+import movies from "https://maylangomes.github.io/cinema-test/public/data/movies.json";
 export async function GET(request: Request) {
   return NextResponse.json(movies);
 }
