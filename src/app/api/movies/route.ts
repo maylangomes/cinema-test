eadebb893ef276b7f90edfe970bc4cecbe3c7842
Maylan <maylan.gomes@epitech.eu>
2024-06-06 11:04:05 +0200

    juste slash

--- src/app/api/movies/route.ts
+++ src/app/api/movies/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import movies from "./public/movies.json";
+import movies from "/movies.json";
 export async function GET(request: Request) {
   return NextResponse.json(movies);
 }
