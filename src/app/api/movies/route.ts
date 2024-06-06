ec63c6a605debb681fbf848c8c7bb27a8f8e2731
Maylan <maylan.gomes@epitech.eu>
2024-06-06 11:18:33 +0200

    worker absolute path

--- src/app/api/movies/route.ts
+++ src/app/api/movies/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import movies from "../../../../public/data/movies.json";
+import movies from "/home/runner/work/cinema-test/cinema-test/public/data/movies.json";
 export async function GET(request: Request) {
   return NextResponse.json(movies);
 }
